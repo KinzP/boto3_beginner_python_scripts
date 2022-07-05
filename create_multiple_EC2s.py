a7e8e89d75bbe9e09ac82bb437981398b161a4df
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-07-05 20:40:54 +0000

    commit 3

--- create_multiple_EC2s.py
+++ create_multiple_EC2s.py
@@ -3,5 +3,5 @@ ec2_resource=boto3.resource("ec2")
 
 ec2_resource.create_instances(ImageId='ami-0cff7528ff583bf9a',
       InstanceType='t2.micro',
-    MaxCount=4,
-      MinCount=3)
+    MaxCount=3,
+      MinCount=2)
