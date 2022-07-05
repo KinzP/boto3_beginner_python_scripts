1784ecbbb77be0326def02e817f0ffc95e3959f2
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-07-05 17:21:08 +0000

    commit 1

---
+++ list_objects_from_s3bucket.py
@@ -0,0 +1,10 @@
+import boto3
+
+s3_resource=boto3.client("s3")
+
+objects=s3_resource.list_objects(Bucket="kparham-bucket-test-boto3")["Contents"]
+
+len(objects)
+
+if len(objects)>0:
+    print("objects exits")
