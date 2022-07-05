e8e24a35874ef1017a71517b526159c6631c7edc
EC2 Default User <ec2-user@ip-10-0-26-220.ec2.internal>
2022-07-05 17:47:13 +0000

    commit 2

--- delete_object.S3.py
+++ delete_object.S3.py
@@ -4,3 +4,7 @@ s3_resource=boto3.client("s3")
 #delete single object
 s3_resource.delete_object(Bucket='kparham-bucket-test-boto3',
       Key='canvas2.png')
+      
+#find all the objects from the bucket
+objects=s3_resource.list_objects(Bucket="kparham-bucket-test-boto3")["Contents"]
+len(objects)
