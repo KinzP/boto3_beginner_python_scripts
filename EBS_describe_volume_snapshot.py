02bb0538d87a09cceffce806e29c044ee48acf67
EC2 Default User <ec2-user@ip-10-0-28-185.ec2.internal>
2022-07-06 16:54:56 +0000

    Final Commit

--- EBS_describe_volume_snapshot.py
+++ EBS_describe_volume_snapshot.py
@@ -1,2 +1,8 @@
+#describe aws ebs volume snapshot using boto3 and python
+
 import boto3
 ec2_boto3=boto3.client("ec2")
+
+ec2_boto3.describe_snapshots(SnapshotIds=[
+          'snap-03f0a437fd2dea30e'
+      ])
